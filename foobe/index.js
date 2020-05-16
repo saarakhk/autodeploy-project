0d79aab09781c99c739cc21901dbdb507e11b6e9
saarakhk <saara.vallik@khk.ee>
2020-05-16 18:22:59 +0300

    second commit

--- foobe/index.js
+++ foobe/index.js
@@ -1,1 +1,10 @@
-<p>test</p>
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3002
+
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Hello World'))
+
+app.listen(port, () => console.log('Example app listening at http://localhost: ${port}'))
